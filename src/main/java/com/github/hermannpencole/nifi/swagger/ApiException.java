5cf04843fb74ad8073df61670c01947e86e54c27
Pencolé Hermann <hermann.pencole.ext@orange.com>
2017-08-09 10:28:05 +0200

    Better usage with java 8 : ApiException is nos RuntimeException

--- src/main/java/com/github/hermannpencole/nifi/swagger/ApiException.java
+++ src/main/java/com/github/hermannpencole/nifi/swagger/ApiException.java
@@ -17,7 +17,7 @@ import java.util.Map;
 import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2017-05-24T23:49:37.943+02:00")
-public class ApiException extends Exception {
+public class ApiException extends RuntimeException {
     private int code = 0;
     private Map<String, List<String>> responseHeaders = null;
     private String responseBody = null;
